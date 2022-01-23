efda9d7c4c94909d58a1bbce6f9613d3f4fedd9c
keke1946 <asadrazanayani@gmail.com>
2022-01-23 01:52:55 -0800

    New changes, Asad

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const myChart = new Chart(ctx, {
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [15, 21, 7, 9, 12, 30],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
